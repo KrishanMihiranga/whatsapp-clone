202e5d79be57e38b81889d0a6662e87d15842713
Krishan <xp3cto95@gmail.com>
2023-11-28 17:34:06 +0530

    fetching images

--- whatsapp/lib/feature/auth/pages/image_picker_page.dart
+++ whatsapp/lib/feature/auth/pages/image_picker_page.dart
@@ -1,4 +1,7 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
 import 'package:whatsapp/common/extension/custom_theme_extension.dart';
 import 'package:whatsapp/common/widgets/custom_icon_button.dart';
 
@@ -10,12 +13,95 @@ class ImagePickerPage extends StatefulWidget {
 }
 
 class _ImagePickerPageState extends State<ImagePickerPage> {
+  List<Widget> imageList = [];
+  int currentPage = 0;
+  int? lastPage ;
+
+
+
+  handleScrollEvent(ScrollNotification scroll) {
+    if (scroll.metrics.pixels / scroll.metrics.maxScrollExtent <= .33) return;
+    if (currentPage == lastPage) return;
+    fetchAllImages();
+  }
+
+  fetchAllImages() async{
+    lastPage = currentPage;
+    final permission = await PhotoManager.requestPermissionExtend();
+    if(!permission.isAuth) return PhotoManager.openSetting();
+
+    List<AssetPathEntity> albums = await PhotoManager.getAssetPathList(
+      type: RequestType.image,
+      onlyAll: true,
+    );
+
+    
+    if (albums.isEmpty) {
+      print('Album is Empty');
+      return;
+    }
+
+    List<AssetEntity> photos = await albums[0].getAssetListPaged(
+      page: currentPage, 
+      size: 24
+    );
+
+    List<Widget> temp = [];
+
+        for (var asset in photos) {
+      temp.add(
+        FutureBuilder(
+          future: asset.thumbnailDataWithSize(
+            const ThumbnailSize(200, 200),
+          ),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: InkWell(
+                  onTap: () => Navigator.pop(context, snapshot.data),
+                  borderRadius: BorderRadius.circular(5),
+                  splashFactory: NoSplash.splashFactory,
+                  child: Container(
+                    margin: const EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: context.theme.greyColor!.withOpacity(.4),
+                        width: 1,
+                      ),
+                      image: DecorationImage(
+                        image: MemoryImage(snapshot.data as Uint8List),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              );
+            }
+            return const SizedBox();
+          },
+        ),
+      );
+    }
+    setState(() {
+      imageList.addAll(temp);
+      currentPage++;
+    });
+  }
+
+  @override
+  void initState() {
+    fetchAllImages();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).backgroundColor,
-        elevation: 0,
+        // elevation: 0,
         leading: CustomIconButton(
           onTap: () => Navigator.pop(context),
           icon: Icons.arrow_back,
@@ -33,6 +119,24 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
           ),
         ],
       ),
-    );  
+       body: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: NotificationListener(
+          onNotification: (ScrollNotification scroll) {
+            handleScrollEvent(scroll);
+            return true;
+          },
+          child: GridView.builder(
+            itemCount: imageList.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+            ),
+            itemBuilder: (_, index) {
+              return imageList[index];
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
